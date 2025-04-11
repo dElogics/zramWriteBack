c493cff10d3538db68536ddaaa97cbf046c62554
de <local>
2025-04-11 20:01:21 +0530

    added systemd unit

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ Script to write to the backing_dev in zram at custom regular intervals.
 
 This script will monitor for memory utilization (excluding the utilization of space as used in the backing_dev) of the zram device and as soon as it reaches the --warnth (a percentage value compared to the total size of the disk) this will start flushing the old data (if it's older than -t seconds) to backing_dev. To do this, this will first write 'all' to idle, then after waiting for -t (defaults to 300s), will write 'idle' to the writeback file.
 
-This script also takes a --criticalth (a percentage value compared to the total size of the disk) and if it sees that the current utilization of memory is more than equal to --criticalth, it will set -t to 1 until percentage zram memory utilization reaches below the --criticalth.
+This script also takes a --criticalth (a percentage value compared to the total size of the disk) and if it sees that the current zram memory utilization is more than equal to --criticalth, it will set -t to 1 until percentage zram memory utilization reaches below the --criticalth.
 
 For zram memory utilization between --warnth and --criticalth, the value of -t will reduce as the zram memory utilization increases (and it approaches --criticalth beyond which it will be set to 1). The difference between --warnth and --criticalth is taken and -t it is divided by this difference and rounded off; the value of -t is reduced by this much for each percentage of zram memory utiliation over --warnth. Of course, -t will return to normal values as soon as it the zram memory utilization is under control.
 
@@ -13,3 +13,8 @@ reduction in -t for zram memory utilization above 60 and upto 90 = 300/30 = 10. 
 For 72% zram memory utilization (which is 12% over the --warnth), this reduction will be 120 seconds or the actual -t will become 180.
 
 This script will do all the above each second (will loop over till eternity). This is a daemon program.
+
+# setup
+The directory structure of this repository resembles that of a rootfs. Put the corresponding files in the corresponding places on your machine to set this up.
+If you've setup the zram device as '0', the start the service as -- 
+```# systemctl daemon-reload; systemctl start zramWriteBack@0.service```
---
+++ usr/bin/zramWriteBack.rb
@@ -0,0 +1,105 @@
+#! /usr/bin/ruby
+require 'optparse'
+
+def parse_options
+	options = {}
+
+	parser = OptionParser.new do |opts|
+		opts.banner = "Usage: example.rb [options]"
+
+		opts.on("-z", "--zram INT", String, "zram device integer (required)") do |z|
+			options[:zram] = z
+		end
+
+		opts.on("-t", "--time [SECONDS]", Integer, "time interval in seconds") do |t|
+			options[:time] = t
+		end
+		
+		opts.on("--warnth [WARN-PERCENTAGE]", String, "Warning threshold value") do |z|
+			options[:warnth] = z
+		end
+		
+		opts.on("--criticalth [CRITICAL-PERCENTAGE]", String, "Critical threshold value") do |z|
+			options[:criticalth] = z
+		end
+
+		opts.on("-h", "--help", "Prints this help") do
+			puts opts
+			exit
+		end
+	end
+
+	begin
+		parser.parse!
+		mandatory = [:zram]
+		missing = mandatory.select { |param| options[param].nil? }
+		unless missing.empty?
+			raise OptionParser::MissingArgument.new(missing.join(', '))
+		end
+	rescue OptionParser::InvalidOption, OptionParser::MissingArgument => e
+		puts "Error: #{e.message}"
+		puts parser
+		exit 1
+	end
+
+	options
+end
+$options = parse_options
+$options[:time] = 300 if $options[:time] == nil
+$options[:criticalth] = 90 if $options[:criticalth] == nil
+$options[:warnth] = 60 if $options[:warnth] == nil
+
+#How much reduction in -t for zram memory utilization between warnth criticalth for each percentage
+$timeReduction = ($options[:time].to_f/($options[:criticalth].to_f - $options[:warnth].to_f)).round
+# Immediate flush (1s gap) if -t is too low.
+$timeReduction = $options[:time] if $timeReduction <= 0
+
+$memstat = File.open("/sys/devices/virtual/block/zram#{$options[:zram]}/mm_stat", "r")
+$maxMem = $memstat.read.scan(/[0-9]+/)[3].to_f
+$memstat.rewind
+def memUsed
+	$memstat.rewind
+	($memstat.read.scan(/[0-9]+/)[2].to_f)/$maxMem*100
+end
+
+$idleFile = File.open("/sys/block/zram#{$options[:zram]}/idle", "w")
+$writebackFile = File.open("/sys/block/zram#{$options[:zram]}/writeback", "w")
+def writeAction
+	puts "DEBUG: Writing to disk because zram memory utilization is #{memUsed}"
+	$idleFile.truncate(0)
+	# Write the content to the file
+	$idleFile.write('all')
+	# Rewind the file pointer to the beginning of the file
+	$idleFile.rewind
+	sleepCounter = 0
+	staleTime = $options[:time]
+	while sleepCounter < staleTime
+		if ($options[:criticalth] - memUsed <= 1)
+			puts "DEBUG: emergency disk write because zram memory utilization too close (or exceeds) criticalth (#{memUsed}%)"
+			sleep 1
+			break
+		else
+# 			If zram memory utilization is lower than critical (but of course higher than warnth)
+			staleTimeReduction = (memUsed - $options[:warnth])*$timeReduction
+			if staleTimeReduction >= staleTime
+				puts "DEBUG: emergency disk write because zram memory utilization too close (or exceeds) criticalth (#{memUsed}%) after compensatory reduction in -t"
+				sleep 1
+				break
+			end
+			staleTime = $options[:time] - staleTimeReduction
+			puts "DEBUG: reduced -t to #{staleTime} because zram memory utilization (#{memUsed}%) exceeds warnth (#{$options[:warnth]})"
+			sleep 1
+			sleepCounter = sleepCounter+1
+		end
+	end
+	$writebackFile.truncate(0)
+	# Write the content to the file
+	$writebackFile.write('idle')
+	# Rewind the file pointer to the beginning of the file
+	$writebackFile.rewind
+end
+
+while true
+	writeAction if memUsed >= $options[:warnth]
+	sleep 1
+end
